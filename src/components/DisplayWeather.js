af6f5e881a07d1bf4d957189999fee76b193ab79
S-Swaroop <itsmesharan2000@gmail.com>
2021-04-11 19:22:33 +0530

    made some css changes

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -6,6 +6,7 @@ function DisplayWeather({data}) {
     return (
         <div className="weather-card">
             <h1>location: {data.name} ({data.sys.country})</h1>
+            <h2>Coordinates : {data.coord.lat}° {data.coord.lon}°</h2>
             <table className="weather-data">
                 <thead>
                     <tr className="headings">
@@ -17,15 +18,14 @@ function DisplayWeather({data}) {
                 </thead>
                 <tbody>
                     <tr className="temperatures">
-                        <td>{data.main.temp}*C</td>
-                        <td>{data.main.feels_like}*C</td>
-                        <td>{data.main.temp_min}*C</td>
-                        <td>{data.main.temp_max}*C</td>
+                        <td>{data.main.temp}°C</td>
+                        <td>{data.main.feels_like}°C</td>
+                        <td>{data.main.temp_min}°C</td>
+                        <td>{data.main.temp_max}°C</td>
                     </tr>
                 </tbody>
             </table>
-            <h2>Conditions: {data.weather[0].main} </h2>
-            <h2></h2>
+            <h2>Conditions: {data.weather[0].main}</h2>   <img className="icon" hight="120px" width="120px" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}></img>
         </div>
     )
 }
